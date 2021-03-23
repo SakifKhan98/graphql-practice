39e5749babeebad2d2c4f785e95b568c21263cfc
SakifKhan98 <sakifkhan98@gmail.com>
2021-03-24 00:48:57 +0600

    Learning Array operator

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
     type Query {
       greeting(name: String, position: String): String!
-      add(a: Float!, b: Float!): Float!
+      add(numbers: [Float!]!): Float!
+      grades: [Int!]!
       me: User!
       post: Post!
     }
@@ -37,7 +38,17 @@ const resolvers = {
       }
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b;
+      if (args.numbers.length === 0) {
+        return 0;
+      }
+
+      // [1, 5, 10, 2]
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      });
+    },
+    grades(parent, args, ctx, info) {
+      return [47, 98, 78, 93, 95];
     },
     me() {
       return {
