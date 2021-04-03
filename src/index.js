62c2f040638a16da1b4b5aa355cfb3aff767e318
SakifKhan98 <sakifkhan98@gmail.com>
2021-04-04 03:31:16 +0600

    implemented comment portion

--- src/index.js
+++ src/index.js
@@ -3,14 +3,98 @@ import { GraphQLServer } from "graphql-yoga";
 // Scalar types - String, Boolean, Int, Float, ID
 // Non-Scalar types - Object, Array
 
+//Demo User Data
+const users = [
+  {
+    id: "1",
+    name: "Sakif",
+    email: "sakif@sakif.com",
+    age: 27,
+  },
+
+  {
+    id: "2",
+    name: "Mike",
+    email: "sakmikef@smikeakif.com",
+    age: 29,
+  },
+
+  {
+    id: "3",
+    name: "Ross",
+    email: "ross@sm.com",
+    age: 23,
+  },
+];
+
+const posts = [
+  {
+    id: "1",
+    title: "GraphQL 101",
+    body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    isPublished: true,
+    author: "1",
+  },
+
+  {
+    id: "2",
+    title: "GraphQL 201",
+    body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    isPublished: false,
+    author: "3",
+  },
+
+  {
+    id: "3",
+    title: "Programming Music",
+    body:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    isPublished: true,
+    author: "2",
+  },
+];
+
+const comments = [
+  {
+    id: "13",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    author: "1",
+    post: "2",
+  },
+  {
+    id: "14",
+    text:
+      " Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    author: "3",
+    post: "1",
+  },
+  {
+    id: "15",
+    text:
+      "Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi!",
+    author: "2",
+    post: "3",
+  },
+  {
+    id: "16",
+    text:
+      "Dolor sit amet consectetur adipisicing elit. Tenetur repellendus vel eum totam architecto, reiciendis molestias sequi! Perspiciatis fuga ipsa veniam, dolorem eveniet cum obcaecati unde, expedita fugit, possimus modi.",
+    author: "1",
+    post: "2",
+  },
+];
+
 // Type definitions (Application Schema)
 const typeDefs = `
     type Query {
-      greeting(name: String, position: String): String!
-      add(numbers: [Float!]!): Float!
-      grades: [Int!]!
+      users(query: String): [User!]!
+      posts(query: String): [Post!]!
       me: User!
       post: Post!
+      comments: [Comment!]!
     }
 
     type User {
@@ -18,37 +102,55 @@ const typeDefs = `
       name: String!
       email: String!
       age: Int
+      posts: [Post!]!
+      comments: [Comment!]!
     }
 
     type Post {
       id: ID!
       title: String!
       body: String!
-      published: Boolean!
+      isPublished: Boolean!
+      author: User!
+      comments: [Comment!]!
+    }
+
+    type Comment {
+      id: ID!
+      text: String!
+      author: User!
+      post: Post!
     }
 `;
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      if (args.name && args.position) {
-        return `Hello, ${args.name}! You are my favorite ${args.position}`;
-      } else {
-        return "Hekki There";
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
       }
-    },
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0;
-      }
-
-      // [1, 5, 10, 2]
-      return args.numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue;
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
       });
     },
-    grades(parent, args, ctx, info) {
-      return [47, 98, 78, 93, 95];
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(args.query.toLowerCase());
+        return isTitleMatch || isBodyMatch;
+      });
+    },
+
+    comments(parent, args, ctx, info) {
+      return comments;
     },
     me() {
       return {
@@ -66,6 +168,42 @@ const resolvers = {
           "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, obcaecati!",
         published: true,
       };
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.post === parent.id;
+      });
+    },
+  },
+  Comment: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
+    },
+    post(parent, args, ctx, info) {
+      return posts.find((post) => {
+        return post.id === parent.post;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
+      });
     },
   },
 };
